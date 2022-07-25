aafdafcb77220d991b19e60ae34f376859ac2996
DevarakondaSaiGanesh <104971284+DevarakondaSaiGanesh@users.noreply.github.com>
2022-07-25 15:59:08 +0530

    Create ASCIIofAllCharacters.java

---
+++ ASCIIofAllCharacters.java
@@ -0,0 +1,13 @@
+
+public class ASCIIofAllCharacters {
+	public static void main(String[] args) 
+	{
+		int i = 0;
+		
+		while(i <= 255)
+		{
+			System.out.println(" The ASCII value of " + (char)i + "  =  " + i);
+			i++;
+		}
+	}
+}
