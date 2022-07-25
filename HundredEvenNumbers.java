b38c12d9733f39dbdc4eb03161439f6475eb2e03
DevarakondaSaiGanesh <104971284+DevarakondaSaiGanesh@users.noreply.github.com>
2022-07-25 15:43:56 +0530

    Create HundredEvenNumbers.java

---
+++ HundredEvenNumbers.java
@@ -0,0 +1,16 @@
+class Even_numbers
+{
+public static void main(String[] args)
+{
+   int i=1;
+   int n=100;
+   while(i<=n)
+   {
+     if(i%2==0)
+     {
+        System.out.print(i+" ");
+     }
+     i++;
+   }
+}
+}
