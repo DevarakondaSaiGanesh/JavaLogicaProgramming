9e56f4a394fb8b5c7c42d4b80fc50dd471de5baa
DevarakondaSaiGanesh <104971284+DevarakondaSaiGanesh@users.noreply.github.com>
2022-07-25 15:43:03 +0530

    Create AtoZ.java

---
+++ AtoZ.java
@@ -0,0 +1,19 @@
+class AtoZ
+{
+ 
+    public static void main(String[] args)
+    {
+        // Declare the variables
+        char i='A';
+ 
+        // Display the alphabets
+        System.out.printf("The Alphabets from A to Z are: \n");
+ 
+        // Traverse each character
+        // with the help of for loop
+       while (i <= 'Z') {
+			System.out.print(i + " ");
+			i++;
+		}
+    }
+}
