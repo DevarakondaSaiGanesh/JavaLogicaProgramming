d89e774bc3b1d383f5463252655d8e9c908b76f2
DevarakondaSaiGanesh <104971284+DevarakondaSaiGanesh@users.noreply.github.com>
2022-07-25 15:49:55 +0530

    Create N_OddNumbers.java

---
+++ N_OddNumbers.java
@@ -0,0 +1,19 @@
+class Even_numbers
+{
+public static void main(String[] args)
+{
+   int i=1;
+   int n=100;
+   int sum=0;
+   while(i<=n)
+   {
+     if(i%2!=0)
+     {
+      sum=sum+i;  
+     }
+     i++;
+   }
+  System.out.println("Sum of all even numbers : "+sum);
+
+}
+}
