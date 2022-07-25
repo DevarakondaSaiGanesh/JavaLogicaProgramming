dc271afe994a038bf7ce4ffcc1a8699fc8163f3d
DevarakondaSaiGanesh <104971284+DevarakondaSaiGanesh@users.noreply.github.com>
2022-07-25 15:44:56 +0530

    Create HundredOddNumbers.java

---
+++ HundredOddNumbers.java
@@ -0,0 +1,16 @@
+class HundredOddNumbers
+{
+public static void main(String[] args)
+{
+   int i=1;
+   int n=100;
+   while(i<=n)
+   {
+     if(i%2!=0)
+     {
+        System.out.print(i+" ");
+     }
+     i++;
+   }
+}
+}
