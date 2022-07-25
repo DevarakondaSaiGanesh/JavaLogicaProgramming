5c26fdaad3de3fc76a4e26692e1e31f79a36b86e
DevarakondaSaiGanesh <104971284+DevarakondaSaiGanesh@users.noreply.github.com>
2022-07-25 15:51:18 +0530

    Create CountDigits.java

---
+++ CountDigits.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class CountDigits
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter a Number");
+      int n=sc.nextInt();
+      int sum=0;
+      int count=0;
+      int temp=n;
+      while(n>0)
+        {
+          temp=n%10;
+          sum=sum+temp;
+          n=n/10;
+          count=count+1;
+        }
+      System.out.println("Count the digits of a Number : "+count);   
+      }
+  }
