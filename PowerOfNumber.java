3f80c7c5e36b03f9a4515442037ab93017a9bfcf
DevarakondaSaiGanesh <104971284+DevarakondaSaiGanesh@users.noreply.github.com>
2022-07-25 15:58:23 +0530

    Create PowerOfNumber.java

---
+++ PowerOfNumber.java
@@ -0,0 +1,10 @@
+class PowerOfNumber {
+  public static void main(String[] args) {
+
+    int base = 3, exponent = 5;
+    double result = Math.pow(base, exponent);
+
+    System.out.println("Answer = " + result);
+  }
+}
+
